aee17f294d50c1a83bf45bbdc0f30844bb2bceb0
giisen <kristerekelund@me.com>
2023-01-12 13:35:01 +0100

    Final

---
+++ CarConsoleClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess;
+using DataAccess.Models;
+
+var carManager = new CarManager();
+
+ConsoleKey key = ConsoleKey.Enter;
+
+while (key != ConsoleKey.Escape)
+{
+    Console.WriteLine("Enter make and model seperated by a space:");
+    var makeModel = Console.ReadLine().Split(' ');
+    var horsePower = 0;
+    bool notInt = false;
+    while (!notInt)
+    {
+
+        Console.WriteLine("Enter horsepower:");
+        if (int.TryParse(Console.ReadLine(), out horsePower))
+        {
+            notInt = true;
+        }
+        else
+        {
+            Console.WriteLine("Input is not a number!");
+        }
+        
+    }
+
+    Console.WriteLine("Enter color:");
+    var color = (Console.ReadLine());
+    Console.WriteLine("Press escape to stop entering cars.");
+    key = Console.ReadKey().Key;
+    var newCar = new Car() { HorsePower = horsePower, Make = makeModel[0], Model = makeModel[1], Color = color };
+
+    carManager.Add(newCar);
+}
+
+var all = carManager.GetAll();
+Console.WriteLine("==========================================");
+
+foreach (var car in all)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Id: {car.Id}");
+    Console.WriteLine($"Make: {car.Make}");
+    Console.WriteLine($"Model: {car.Model}");
+    Console.WriteLine($"Color: {car.Color}");
+    Console.WriteLine($"Horsepower: {car.HorsePower}");
+    Console.WriteLine();
+}
